48d1dbd717d7c07ab4dd4bd4173cfbec1f62c9fb
ryannn13 <ryan.hidayat0604@gmail.com>
2024-11-04 00:43:44 +0700

    Model

---
+++ app/Models/Post.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Post
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Ryan Hidayat',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatum aut fugit optio est provident excepturi ea beatae iusto eveniet neque nam sint nesciunt recusandae atque ex eos, minima cum!'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Ryan Hidayat',
+                'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque laudantium sed dignissimos, nostrum saepe debitis reiciendis, accusamus placeat eligendi velit autem consectetur suscipit ratione fugiat deserunt sint porro earum? Voluptatibus!'
+            ]
+        ];
+    }
+
+    public static function find($slug)
+    {
+        // return Arr::first(static::all(), function ($post) use ($slug) {
+        //     return $post['slug'] == $slug;
+        // });
+
+        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
+
+        if (!$post) {
+            abort(404);
+        }
+
+        return $post;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -14,45 +15,13 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', [
         'title' => 'Blog Page',
-        'posts' => [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Ryan Hidayat',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatum aut fugit optio est provident excepturi ea beatae iusto eveniet neque nam sint nesciunt recusandae atque ex eos, minima cum!'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Ryan Hidayat',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque laudantium sed dignissimos, nostrum saepe debitis reiciendis, accusamus placeat eligendi velit autem consectetur suscipit ratione fugiat deserunt sint porro earum? Voluptatibus!'
-        ]
+        'posts' => Post::all()
     ]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        'title' => 'Blog Page',
-        'posts' => [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Ryan Hidayat',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia voluptatum aut fugit optio est provident excepturi ea beatae iusto eveniet neque nam sint nesciunt recusandae atque ex eos, minima cum!'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Ryan Hidayat',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque laudantium sed dignissimos, nostrum saepe debitis reiciendis, accusamus placeat eligendi velit autem consectetur suscipit ratione fugiat deserunt sint porro earum? Voluptatibus!'
-        ]
-    ];
 
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+    $post = Post::find($slug)
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
