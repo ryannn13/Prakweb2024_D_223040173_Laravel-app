db212011d15a497e97ab1919f5e9a44306b88186
ryannn13 <ryan.hidayat0604@gmail.com>
2024-11-06 17:19:41 +0700

    Post Category

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -25,9 +26,14 @@ Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
-Route::get('/authors/{user}', function (User $user) {
+Route::get('/authors/{user:username}', function (User $user) {
 
     return view('posts', ['title' => 'Articles by ' . $user->name, 'post' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+
+    return view('posts', ['title' => 'Articles in ' . $category->name, 'post' => $category->posts]);
 });
 
 Route::get('/contact', function () {
